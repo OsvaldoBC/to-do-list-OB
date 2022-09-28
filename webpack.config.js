20d4438858bdd7e28ffc2090a0a3e8653aa34010
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-09-27 22:47:54 -0500

    Add linters styles

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-    template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
   mode: 'development',
@@ -18,15 +18,16 @@ module.exports = {
     clean: true,
     publicPath: '/',
   },
- // optimization: {
- //   runtimeChunk: 'single',
- // },
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+
     ],
   },
 };
