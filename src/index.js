26a12061a05b42fad7b5d3b9776687de7f0e6275
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-09-29 19:43:33 -0500

    Add interactive features list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash'; // eslint-disable-line
 import './style.css';
 import displayTasks from './modules/3-display.js';
 import addTask from './modules/2-addTask.js';
+import clearTasks from './modules/4-clear.js';
 
 const form = document.querySelector('.form');
 
@@ -9,6 +10,14 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const input = Object.fromEntries(new FormData(e.target));
   addTask(input.task);
+  displayTasks();
+});
+
+const clearBtn = document.querySelector('.clr-all');
+
+clearBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  clearTasks();
   displayTasks();
 });
 
